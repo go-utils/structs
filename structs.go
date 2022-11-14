7ceb8d1c9dff9b0e10c91308cc8b29546bd1fc24
54m <30588003+54m@users.noreply.github.com>
2022-11-14 19:40:40 +0900

    add: `GetNilFields` func

--- structs.go
+++ structs.go
@@ -24,3 +24,58 @@ func GetStructName(obj any) string {
 
 	return rt.Name()
 }
+
+// GetNilFields - get nil field names
+func GetNilFields(obj any) []string {
+	return getNilFields(obj, "")
+}
+
+func getNilFields(value any, parent string) []string {
+	nilFields := make([]string, 0)
+
+	rv := reflect.ValueOf(value)
+	switch rv.Kind() {
+	case reflect.Struct:
+		// ok
+	case reflect.Ptr:
+		if rv.IsNil() {
+			return append(nilFields, parent)
+		}
+	default:
+		return nilFields
+	}
+
+	rv = reflect.Indirect(rv)
+	rt := rv.Type()
+	structName := rt.Name()
+	for i := 0; i < rt.NumField(); i++ {
+		f := rt.Field(i)
+		fieldName := f.Name
+
+		field := rv.FieldByName(fieldName)
+		if !field.IsValid() {
+			continue
+		}
+
+		key := structName + "." + fieldName
+		if parent != "" {
+			key = parent + "." + key
+		}
+
+		switch field.Kind() {
+		case reflect.Ptr, reflect.Interface:
+			if field.IsNil() {
+				nilFields = append(nilFields, key)
+				continue
+			}
+		}
+
+		if f.Anonymous {
+			nilFields = append(nilFields, getNilFields(field.Interface(), structName)...)
+		}
+
+		continue
+	}
+
+	return nilFields
+}
